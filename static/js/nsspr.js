7ea25b4dea99775795eb820c706e0430c54fc29c
Jeff Franklin <jpf@uw.edu>
2015-06-30 16:04:06 -0700

    a bare-bones django site that runs with identity styling (NETID-271)

---
+++ static/js/nsspr.js
@@ -0,0 +1,8 @@
+// possible js tools for the project
+
+var app = angular.module('nssprApp', []);
+
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+}]);
