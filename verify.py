152f8b4fbf1e696e3ac3931cb655d3b2a5abbd88
Ann Testroet <annt@uw.edu>
2015-08-18 11:45:21 -0700

    Deleted verify.py file.

--- verify.py
+++
@@ -1,7 +0,0 @@
-from resttools.irws import IRWS
-from django.conf import settings
-
-def match_student_pac(uwnetid, pac):
-    irws = IRWS(settings.IRWS_CONF)
-    response = irws.get_person(netid=uwnetid)
-    return False
