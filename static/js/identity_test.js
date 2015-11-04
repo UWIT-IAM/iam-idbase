a8bdb884b99663afe71af0cdd8150118906ea463
jimfox <fox@washington.edu>
2015-11-03 16:01:12 -0800

    Adding more identity.js tests; starting configure.js tests

--- static/js/identity_test.js
+++ static/js/identity_test.js
@@ -8,24 +8,43 @@ describe('identity common tools', function() {
     $controller = _$controller_;
   }));
 
+  beforeEach(inject(function() {
+    var $injector = angular.injector(['identityApp']);
+    TitleSvc = $injector.get('TitleSvc');
+    ErrorSvc = $injector.get('ErrorSvc');
+    TimeoutSvc = $injector.get('TimeoutSvc');
+  }));
+
+  beforeEach(inject(function (_$http_, _$log_, _$location_, _$rootScope_, _ErrorSvc_) {
+        $http = _$http_;
+        $log = _$log_;
+        $location = _$location_;
+        $rootScope = _$rootScope_;
+        ErrorSvc = _ErrorSvc_;
+    }));
+
   describe('common controllers', function(){
 
-    it('has title controller', function() {
-      var ctrl = $controller('TitleCtrl');
+    it('title controller returns default title', function() {
+      var ctrl = $controller('TitleCtrl', { TitleSvc: TitleSvc} );
       expect(ctrl).toBeDefined();
+      expect(ctrl.Page.title()).toEqual('Identity.UW');
     });
 
     it('has timeout controller', function() {
-      //spec body
-      var ctrl = $controller('TimeoutCtrl');
+      var ctrl = $controller('TimeoutCtrl', {TimeoutSvc: TimeoutSvc});
       expect(ctrl).toBeDefined();
     });
 
-    it('has error controller', function() {
-      //spec body
-      var ctrl = $controller('ErrorCtrl');
+    it('error factory sets error state', function() {
+      var ctrl = $controller('ErrorCtrl', {ErrorSvc: ErrorSvc});
       expect(ctrl).toBeDefined();
+      ErrorSvc.handleError('aaa', 404);
+      expect(ErrorSvc.state.isErrorSet).not.toBeTruthy();
+      ErrorSvc.handleError('aaa', 500);
+      expect(ErrorSvc.state.isErrorSet).toBeTruthy();
     });
+
 
   });
 });
