1e99ae1283de714ae935aeae559db62856886fb1
Jeff Franklin <jpf@uw.edu>
2015-12-22 17:00:47 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+install_requires = ['Django', 'django-compressor']
+
+setup(name='idbase',
+      version='0.1',
+      description='Identity.UW base look and feel',
+      install_requires=install_requires,
+      )
