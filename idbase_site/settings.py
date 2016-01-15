f776210171eaeb5aa9ee9452b76c7d673f8f2083
Jeff Franklin <jpf@uw.edu>
2016-01-15 11:28:22 -0800

    Tweak the tab style and get rid of css files we don't want to support.

--- idbase_site/settings.py
+++ idbase_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cdk%f))1=8(@wt!%+!s4#ph*-oq_-#ay6b!)@yvnmw()4gw#1n'
+SECRET_KEY = 'load this secret externally'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
