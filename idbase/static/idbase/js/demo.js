c2cb25d2b40ff0707db47c52d48bbe7d14f2435e
Jeff Franklin <jpf@uw.edu>
2016-03-10 17:48:26 -0800

    Fix missing error message.

--- idbase/static/idbase/js/demo.js
+++ idbase/static/idbase/js/demo.js
@@ -1,2 +1,8 @@
 // javascript file for demo purposes only
 var app = angular.module('identityApp');
+
+app.controller('MakeErrorCtrl', ['ErrorSvc', function(ErrorSvc){
+    this.doIt = function() {
+        ErrorSvc.state.isErrorSet = true;
+    };
+}]);
