3bd2978142997e5cd28d2a08085bf5b232e7d6fa
Jeff Franklin <jpf@uw.edu>
2016-03-11 10:09:01 -0800

    idbase version 1.2.0.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-
+import idbase
 
 install_requires = ['Django', 'pytz']
 
 setup(name='idbase',
-      version='0.1',
+      version=idbase.__version__,
       description='Identity.UW base look and feel',
       install_requires=install_requires,
       )
